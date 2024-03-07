3469ab6ad42b0e089dbdac6b4530dad4c1921906
Berkay INAN <inan.berkay@outlook.com>
2024-03-08 00:01:01 +0300

    feature-readrepository-dbcontext

---
+++ HR_Project.Persistence/Context/HRProjectAPIDBContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace HR_Project.Persistence.Context
+{
+    public class HRProjectAPIDBContext : IdentityDbContext <AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Spend> Spends { get; set; }
+        public DbSet<Leave> Leaves { get; set; }
+        public DbSet<Advance> Advances { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<SiteOwner> SiteOwners { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
