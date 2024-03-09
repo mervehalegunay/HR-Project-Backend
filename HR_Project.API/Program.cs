9851ffb7b2e34645134e1e708a59e3a6610c1177
Berkay INAN <inan.berkay@outlook.com>
2024-03-10 02:10:41 +0300

    deneme

--- HR_Project.API/Program.cs
+++ HR_Project.API/Program.cs
@@ -3,9 +3,12 @@ using HR_Project.Application.Services;
 using HR_Project.Domain.Entitites.Common;
 using HR_Project.Persistence.Context;
 using HR_Project.Persistence.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Text;
 
 namespace HR_Project.API
 {
@@ -15,31 +18,78 @@ namespace HR_Project.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+            builder.Services.AddDbContext<HRProjectAPIDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conStr")));
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 8;
+                opts.Password.RequireUppercase = true;
+                opts.Password.RequireDigit = true;
+                opts.Password.RequireLowercase = true;
+                opts.Password.RequireNonAlphanumeric = true;
+
+            }).AddEntityFrameworkStores<HRProjectAPIDBContext>().AddDefaultTokenProviders();
+
+
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+            var secretKey = jwtSettings["secretKey"];
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = jwtSettings["validIssuer"],
+                    ValidAudience = jwtSettings["validAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+
+            });
+
+            //REPOSÝTORÝES
             builder.Services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-            builder.Services.AddTransient<ISiteManagerRepository , SiteManagerRepository>();
 
-            // builder.Services.AddTransient(typeof(IReadRepository<>), typeof(ReadRepository<>));
-            
+            builder.Services.AddTransient<ISiteManagerRepository, SiteManagerRepository>();
 
-            
-            
-            builder.Services.AddTransient<SiteManagerService>();
+
+ 
+            //builder.Services.AddTransient<ISiteManagerService, SiteManagerService>();
 
 
 
 
-            // Add services to the container.
 
 
-            builder.Services.AddDbContext<HRProjectAPIDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Tarik")));
 
-
-            builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<HRProjectAPIDBContext>();
-
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("https://localhost:5173") // Ýzin vermek istediðiniz kaynak
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
 
             var app = builder.Build();
 
