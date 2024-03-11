744c9afe34d68d0fe10ddd9798f06d7a83e83eed
Tarkovski7 <trkbgr1618@gmail.com>
2024-03-11 07:50:57 +0300

    updated

--- HR_Project.API/Program.cs
+++ HR_Project.API/Program.cs
@@ -1,92 +1,64 @@
-using HR_Project.Application;
-using HR_Project.Domain.Entitites.Common;
-using HR_Project.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using HR_Project.Persistence.Context;
 using HR_Project.Persistence.Repositories;
+using HR_Project.Application;
+using HR_Project.Domain.Entitites.Common;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
-namespace HR_Project.API
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HRProjectAPIDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Tarik")));
+
+// builder.Services.AddPersistenceServices(); 
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<HRProjectAPIDBContext>();
+
+builder.Services.ConfigureApplicationCookie(opts => opts.LoginPath = "/api/account/login");
+
+builder.Services.Configure<IdentityOptions>(opt =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    opt.User.RequireUniqueEmail = true;
+});
+
+builder.Services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddTransient<ISiteManagerRepository, SiteManagerRepository>();
+builder.Services.AddTransient<PasswordHasher<AppUser>>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<HRProjectAPIDBContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("Tarik")));
+            builder.WithOrigins("https://localhost:7244")
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
-            builder.Services.AddPersistenceServices(); // bu kod repolar� servisleri falan tek yerden ioc olarak kullanmam�z� sagl�yo bunun i�ini de persistence katman�ndaki serviceregistration da dolduruyoruz
+var app = builder.Build();
 
-
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-
-
-            builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<HRProjectAPIDBContext>();
-
-            builder.Services.ConfigureApplicationCookie(opts => opts.LoginPath = "/Account/Login");
-
-            builder.Services.Configure<IdentityOptions>(opt =>
-            {
-                opt.User.RequireUniqueEmail = true;
-            });
-
-
-
-
-
-
-            //REPOS�TOR�ES
-            builder.Services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-
-            builder.Services.AddTransient<ISiteManagerRepository, SiteManagerRepository>();
-            builder.Services.AddTransient<PasswordHasher<AppUser>>();
-
-
-            //builder.Services.AddTransient<ISiteManagerService, SiteManagerService>();
-
-
-
-
-
-
-
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddCors(options =>
-            {
-               options.AddPolicy("AllowSpecificOrigin",
-                   builder =>
-                   {
-                       builder.WithOrigins("https://localhost:5173") // �zin vermek istedi�iniz kaynak
-                              .AllowAnyMethod()
-                              .AllowAnyHeader();
-                   });
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
